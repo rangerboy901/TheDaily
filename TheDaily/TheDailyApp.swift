c872bac2866a2626827e8bf2093df5198ec05843
Joseph DeWeese <rangerboy901@icloud.com>
2022-06-14 23:02:51 -0500

    Initial Commit

---
+++ TheDaily/TheDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheDailyApp.swift
+//  TheDaily
+//
+//  Created by Joseph DeWeese on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct TheDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
